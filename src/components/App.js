d50ef481fb8ab594faeafa97f977b9412b40c42a
DatNguyen <vovanguyen11@gmail.com>
2020-09-21 12:07:25 +0300

    done header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react"
+import {Route, Switch} from "react-router-dom"
 
-function App() {
-  return <div className="app">Hello React</div>
+import HomePage from "../pages/HomePage"
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="app">
+        <div className="content">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
